932687ba071183cbd837ac38d9a4bdabf369f874
KH <littlekarrie@gmail.com>
2019-07-17 17:20:22 -0700

    Updates

---
+++ precache-manifest.b17252a7da380c5298a24690b9892aba.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9fbf4ab88b450a5effa",
+    "url": "/interesting-charts/static/css/main.c6628975.chunk.css"
+  },
+  {
+    "revision": "d9fbf4ab88b450a5effa",
+    "url": "/interesting-charts/static/js/main.10845ee9.chunk.js"
+  },
+  {
+    "revision": "841339bd17a2ad42491d",
+    "url": "/interesting-charts/static/js/runtime~main.aa7c395f.js"
+  },
+  {
+    "revision": "6f926ee42710a6e39533",
+    "url": "/interesting-charts/static/css/2.9052e6f0.chunk.css"
+  },
+  {
+    "revision": "6f926ee42710a6e39533",
+    "url": "/interesting-charts/static/js/2.cf974ca9.chunk.js"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/interesting-charts/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/interesting-charts/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/interesting-charts/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/interesting-charts/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/interesting-charts/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/interesting-charts/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/interesting-charts/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/interesting-charts/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/interesting-charts/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/interesting-charts/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/interesting-charts/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/interesting-charts/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/interesting-charts/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/interesting-charts/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/interesting-charts/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/interesting-charts/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "f71444d605dc9567a43ebb59c40c96bb",
+    "url": "/interesting-charts/index.html"
+  }
+];
